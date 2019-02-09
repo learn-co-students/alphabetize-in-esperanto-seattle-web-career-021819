7cea2a0cbe42b2b3fcd69d4baf4c8588c91e760d
github username <github email address>
2019-02-09 14:17:08 -0800

    alphabetize solution

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+
+ arr.sort_by{|element| element.chars.collect {|letter| ESPERANTO_ALPHABET.index(letter)}}
+
 end
+
+#go through original array and for each element
+#action on the block (starts w element.chars.collect)
+#element.chars.collect will return a new array w values returned by block
+#which is an array of indexes for each letter in the esp alphabet
+#then everything is sorted ascendingly and original elements are returned alphabetically
